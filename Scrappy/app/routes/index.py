0e1be456171a6398ef9ad817fcb48ecd56ec7361
Alpha Diallo <alpha2779@gmail.com>
2023-10-11 21:52:04 +0400

    Worked on some ui in the index page and the res.html, and added the detail history modal

--- Scrappy/app/routes/index.py
+++ Scrappy/app/routes/index.py
@@ -1,5 +1,5 @@
 # app/routes/index.py
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 
 bp = Blueprint('index', __name__)
@@ -8,4 +8,4 @@ bp = Blueprint('index', __name__)
 @bp.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    return render_template('index.html',  current_endpoint=request.endpoint)
--- Scrappy/app/routes/sample_history_routes.py
+++ Scrappy/app/routes/sample_history_routes.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify, Response
 from flask_login import login_required, current_user
 from app.models.website import Website
+from app.models.sample import Sample
+import json
+import ast
 
 
 bp = Blueprint('sample_history', __name__)
@@ -13,4 +16,21 @@ def history():
     websites = Website.get_by_user_id(current_user.id)
     
     # Pass the websites to the template
-    return render_template('history.html', websites=websites)
+    return render_template('history.html', websites=websites, current_endpoint=request.endpoint)
+
+@bp.route('/history/<int:website_id>', methods=['GET'])
+@login_required
+def get_sample_data(website_id):
+    sample = Sample.query.filter_by(website_id=website_id).first()
+    if sample:
+        try:
+            # Assuming that sample.sample_data is a stringified JSON
+            # We parse it into a Python object
+            data_dict = ast.literal_eval(sample.sample_data)
+            return Response(json.dumps(data_dict), mimetype='application/json')
+        except json.JSONDecodeError:
+            # Log the error for debugging
+            current_app.logger.error(f"Failed to decode JSON for sample id {sample.id}")
+            return jsonify(error="Failed to decode sample data"), 500
+    return jsonify([])  # return empty list if no data
+
